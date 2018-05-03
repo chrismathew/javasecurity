2cc2095d90c4716cdaeca61c6929be2f219f8d06
Chris Mathew <cmathew06@gamil.com>
2018-05-03 11:26:02 -0400

    spring mvc security

---
+++ ConfigSpringMvcSecurity/src/main/java/com/chris/security/ConfigSpringMvcSecurityApplication.java
@@ -0,0 +1,12 @@
+package com.chris.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigSpringMvcSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigSpringMvcSecurityApplication.class, args);
+	}
+}
